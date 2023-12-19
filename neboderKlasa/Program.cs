91db58a95474565009aeb8723233e76a2739b85c
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-12-19 10:41:04 +0100

    Add files via upload

---
+++ neboderKlasa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neboderKlasa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            neboder neboder = new neboder(200+10, 100+1);
+
+            Console.WriteLine(neboder.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ neboderKlasa/neboder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neboderKlasa
+{
+    internal class neboder
+    {
+        float visina, katovi;
+
+
+        public void setVisina(int visina)
+        {
+            this.visina = visina;
+        }
+        public float GetVisina()
+        {
+            return this.visina;
+        }
+
+        public void setKatovi(int katovi)
+        {
+            this.katovi = katovi;
+        }
+        public float GetKatovi()
+        {
+            return this.katovi;
+        }
+
+        public override string ToString()
+        {
+            string ispis = "Visina: " + this.GetVisina() + "m\n" + "katovi: " + this.GetKatovi() + "\n"
+                + "Prosijek je --> " + this.GetVisina() / this.GetKatovi() + " kata";
+            return ispis;
+        }
+        public neboder(int visina, int katovi) { this.visina = visina; this.katovi = katovi; }
+
+    }
+}
